76e61cb11746ae03595fecf434e6253ab692744e
Tais <dtais@outlook.com.br>
2023-10-25 23:18:48 -0300

    mix_coluns

--- aes.py
+++ aes.py
@@ -1,22 +1,21 @@
-
-S_BOX = (
-    0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
-    0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
-    0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
-    0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
-    0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
-    0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
-    0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
-    0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
-    0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
-    0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
-    0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
-    0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
-    0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
-    0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
-    0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
-    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
-  )
+SBOX = [
+    ["63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76"],
+    ["CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C", "A4", "72", "C0"],
+    ["B7", "FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31", "15"],
+    ["04", "C7", "23", "C3", "18", "96", "05", "9A", "07", "12", "80", "E2", "EB", "27", "B2", "75"],
+    ["09", "83", "2C", "1A", "1B", "6E", "5A", "A0", "52", "3B", "D6", "B3", "29", "E3", "2F", "84"],
+    ["53", "D1", "00", "ED", "20", "FC", "B1", "5B", "6A", "CB", "BE", "39", "4A", "4C", "58", "CF"],
+    ["D0", "EF", "AA", "FB", "43", "4D", "33", "85", "45", "F9", "02", "7F", "50", "3C", "9F", "A8"],
+    ["51", "A3", "40", "8F", "92", "9D", "38", "F5", "BC", "B6", "DA", "21", "10", "FF", "F3", "D2"],
+    ["CD", "0C", "13", "EC", "5F", "97", "44", "17", "C4", "A7", "7E", "3D", "64", "5D", "19", "73"],
+    ["60", "81", "4F", "DC", "22", "2A", "90", "88", "46", "EE", "B8", "14", "DE", "5E", "0B", "DB"],
+    ["E0", "32", "3A", "0A", "49", "06", "24", "5C", "C2", "D3", "AC", "62", "91", "95", "E4", "79"],
+    ["E7", "C8", "37", "6D", "8D", "D5", "4E", "A9", "6C", "56", "F4", "EA", "65", "7A", "AE", "08"],
+    ["BA", "78", "25", "2E", "1C", "A6", "B4", "C6", "E8", "DD", "74", "1F", "4B", "BD", "8B", "8A"],
+    ["70", "3E", "B5", "66", "48", "03", "F6", "0E", "61", "35", "57", "B9", "86", "C1", "1D", "9E"],
+    ["E1", "F8", "98", "11", "69", "D9", "8E", "94", "9B", "1E", "87", "E9", "CE", "55", "28", "DF"],
+    ["8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16"]
+]
 
 
 InvSbox = (
@@ -43,30 +42,39 @@ Rcon = (
     0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A, 0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39,
   )
 
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
+
 
 # Função de substituição S-Box
-def sub_bytes(state):
+def sub_bytes(matrix):
   for i in range(4):
     for j in range(4):
-      state[i][j] = S_BOX[state[i][j]]
+      x = int(matrix[i][j][0],16)
+      y = int(matrix[i][j][1],16)
+      matrix[i][j] = SBOX[x][y]
+  return matrix
 
 # Função de permutação de linhas
-def shift_rows(state):
-  for i in range(4):
-    state[i] = state[i][i:] + state[i][:i]
+def shift_rows(est):
+  est[0][1], est[1][1], est[2][1], est[3][1] = est[1][1], est[2][1], est[3][1], est[0][1]
+  est[0][2], est[1][2], est[2][2], est[3][2] = est[2][2], est[3][2], est[0][2], est[1][2]
+  est[0][3], est[1][3], est[2][3], est[3][3] = est[3][3], est[0][3], est[1][3], est[2][3]
+  return est
+
 
 def mix_columns(state):
   for i in range(4):
-    s0 = state[i][0]
-    s1 = state[i][1]
-    s2 = state[i][2]
-    s3 = state[i][3]
+    s0 = int(state[i][0],16)
+    s1 = int(state[i][1],16)
+    s2 = int(state[i][2],16)
+    s3 = int(state[i][3],16)
 
     state[i][0] = (2 * s0) ^ (3 * s1) ^ s2 ^ s3
     state[i][1] = s0 ^ (2 * s1) ^ (3 * s2) ^ s3
     state[i][2] = s0 ^ s1 ^ (2 * s2) ^ (3 * s3)
     state[i][3] = (3 * s0) ^ s1 ^ s2 ^ (2 * s3)
 
+  return state
 
 
 def key_expansion(key, round):
@@ -83,7 +91,7 @@ def key_expansion(key, round):
             temp = temp[1:] + [temp[0]]
 
             # Substituição de bytes (usando a tabela S-box)
-            temp = [S_BOX[b] for b in temp]
+            temp = [SBOX[b] for b in temp]
 
             # Realizar operação XOR com a rodada do Rcon
             temp[0] ^= RCON[i // 4]
@@ -134,42 +142,6 @@ def aes_encrypt(plaintext, key, num_rounds=10):
     return [state[i][j] for i in range(4) for j in range(4)]
 
 
-def to_hex(text):
-  string_hex = []
-  for i in text:
-    string_hex.append(hex(ord(i)))
-  return(string_hex)
-
-def to_matrix(text):
-  lista_matriz = []
-
-  aux = len(text) % 16
-  if aux:
-    text += 'd6' * ((16 - aux)/2)
-
-  n = 0
-
-  for k in range(0, len(text), 32):
-    matriz = []
-    for i in range(16):
-    # two hex characters == 1 byte
-      byte = (text[n*2:n*2+2])
-      #byte = int(text[n*2:n*2+2], 16)
-      try:
-        byte = int(byte,16)
-      except:
-        pass
-      if i % 4 == 0:
-         matriz.append([byte])
-      else:
-         matriz[i // 4].append(byte)
-      n += 1
-    lista_matriz.append(matriz)
-
-       
-
-  return lista_matriz
-
 def to_text(data):
   text = ""
   for k in range(len(data)):
@@ -183,34 +155,90 @@ def to_text(data):
 
   return text
 
-# def addPad(data):
-#   bits = []
-#   while(True):
-#     if(len(data) > bytes):
-#       bits.append(data[:bytes])
-#       data = data[bytes:]
-#     else:
-#       space = bytes-len(data)
-#       bits.append(data+chr(space)*space)
-#       break
-#   return bits
+#AddRoundKey
+#SubBytes
+#shift_rows
+#AddRoundKwy
 
 
-def encrypt(mensagem, chave):
-  print(mensagem)
-  data = to_matrix(mensagem)
-  print(data)
-  text = to_text(data)
-  print(text)
+
+def change_key(chave):
+  round_key = to_matrix(chave)
+  for i in range(4, 4*11):
+    round_key.append([])
+    if i % 4 == 0:
+      byte = round_key[i - 4][0] ^ S_BOX[round_key[i-1][1]] ^ Rcon[i/4]
+      round_key[i].append(byte)
+      for j in range(1,4):
+        byte = round_key[i - 4][j] ^ S_BOX[round_key[i-1][(j + 1) % 4]]
+        round_key[i].append(byte)
+    else:
+      byte = round_key[i - 4][j] ^ round_key[i-1][j]
+      round_key[i].append(byte)
+
+
+def encrypt(matrix, chave):
+  dado = sub_bytes(matrix)
+  dado = shift_rows(dado)
+  dado = mix_columns(dado)
+  print(dado)
+
+
+  # data = to_matrix(mensagem)
+  # print(data)
+  # text = to_text(data)
+  # print(text)
   pass
 
 def descrypt(arquivo):
   #todo
   pass
 
+def to_matrix(bloco):
+  matrix = []
+  cont = 0
+  for i in range(4):
+    linha = []
+    for j in range(4):
+      linha.append(bloco[cont])
+      cont += 1
+    matrix.append(linha)
+  print(matrix)
+  return matrix
+
+def divideBlocks(lista):
+  blocos = []
+  cont = 0 
+
+  if len(lista) % 16 != 0:
+    for i in range(16 -(len(lista) % 16)):
+      lista.append('00')
+
+  n = len(lista) // 16
+  
+  for i in range(n):
+    bloco = []
+    for j in range(16):
+      bloco.append(lista[cont])
+      cont+= 1
+    blocos.append(bloco)
+  return blocos
+  
+def breakString(mensagem):
+  lista = []
+  for c in range(0,len(mensagem),2):
+    lista.append(mensagem[c]+mensagem[c+1])
+  return lista
+
 def aes(arquivo,valor,chave):
   if valor == 1:
-    encrypt(arquivo, chave)
+    lista_hex = breakString(arquivo)
+    blocos = divideBlocks(lista_hex)
+    lista_matrix = []
+    for bloco in blocos:     
+      lista_matrix.append(to_matrix(bloco))
+    for matrix in lista_matrix:
+      encrypt(matrix, chave)
   else:
     descrypt(arquivo, chave)
 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def open_file(file_path):
     try:
         with open(file_path, "rb") as file:
           data = base64.b16encode(file.read())
-          data = str(data, "utf-8").lower()
+          data = str(data,"ascii").upper()
           return data       
     except Exception as e:
         print(f"Não foi possível ler o arquivo: {e}")
@@ -16,5 +16,6 @@ def open_file(file_path):
 caminho_arquivo = input("Informe o caminho do arquivo: ")
 
 arquivo = open_file(caminho_arquivo)
+print(arquivo)
 chave = 0x2b7e151628aed2a6abf7158809cf4f3c
 aes(arquivo, 1, chave)
